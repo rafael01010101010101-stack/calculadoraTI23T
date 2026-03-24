a4ffa9210d9e5ed3a073b6e7ff9cd784b1f7bf52
RAFAEL DE BARROS ALMEIDA <rafael.balmeida3@senacsp.edu.br>
2026-03-24 17:22:42 -0300

    Segundo Commit

--- CalculadoraTI23T/ControlCalculadora.cs
+++ CalculadoraTI23T/ControlCalculadora.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,14 +26,23 @@ namespace CalculadoraTI23T
         //Metodo de menu
         public string mostrarMenu() 
         {
-            return "--------Menu--------\n\n"       +
+            return "--------Menu--------\n\n" +
                 "Escolha uma das opções abaixo: \n" +
-                "0. Sair\n"                         +
-                "1. Somar\n"                        +
-                "2. Subtrair\n"                     +
-                "3. Multiplicar\n"                  +
-                "4. Dividir\n"                      +
-                "5. Potência\n";                
+                "0. Sair\n" +
+                "1. Somar\n" +
+                "2. Subtrair\n" +
+                "3. Multiplicar\n" +
+                "4. Dividir\n" +
+                "5. Potência\n" +
+                "6. Raiz do Primeiro Numero\n" +
+                "7. Raiz do Segundo Numero\n" +
+                "8. Tabuada\n" +
+                "9. Ano bissexto\n"+
+                "10. Entre 100 e 200\n"+
+                "11. Verificar se pode votar\n"+
+                "12. Verificar se esta dentro de um intervalo\n"+
+                "13. Leia um número e verifique se ele esta entre os 10 maiores lidos";
+
         }//fim do metodo
 
         public void realizarOperacao() 
@@ -42,25 +52,25 @@ namespace CalculadoraTI23T
             {
                 Console.WriteLine(this.mostrarMenu());//chamar menu
                 opcao = Convert.ToInt32(Console.ReadLine());//leia
-                if (opcao != 0) 
+                if (opcao != 0 && opcao !=6 && opcao !=7 && opcao !=8 && opcao !=9 && opcao != 10 && opcao !=11 && opcao !=12 && opcao !=13) 
                 {
                     this.coletar();//pegar oque o usuario esta digitando
                 }
                 switch (opcao)
                 { 
-                            case 0:
+                        case 0:
                                 Console.WriteLine("Obrigado!");
                             break;
-                            case 1:
+                        case 1:
                                 Console.WriteLine("A soma é:" +this.model.somar());
                             break;
-                            case 2:
+                        case 2:
                                 Console.WriteLine("A substração é:" + this.model.subtrair());
                             break;
-                            case 3:
+                        case 3:
                                 Console.WriteLine("A mutiplicação é:" + this.model.multiplicar());
                             break;
-                            case 4:
+                        case 4:
                                 if (this.model.dividir() == -1)
                                 {
                                     Console.WriteLine("Impossivel dividir por zero!!");
@@ -70,9 +80,59 @@ namespace CalculadoraTI23T
                                     Console.WriteLine("A divisão é:" + this.model.dividir());
                                 }
                             break;
-                            case 5:
-                                Console.WriteLine("A potência é: " + this.model.potencia());
-                            break;
+                        case 5:
+                            
+                            Console.WriteLine("A potência é: " + this.model.potencia());
+                        break;
+                        case 6:
+                            Console.WriteLine("Informe um numero: ");
+                            this.model.setNum1(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("A Raiz do Primeiro numero é: " + this.model.raizPrimeiroNumero());
+                        break;
+                        case 7:
+                            Console.WriteLine("Informe um numero: ");
+                            this.model.setNum1(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("A Raiz do Segundo numero é: " + this.model.raizSegundoNumero());
+                        break;
+                        case 8:
+                            Console.WriteLine("Informe um numero: ");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            //Para chamar a tabuada
+                            Console.WriteLine(this.model.tabuada(num));
+                        break;
+                        case 9:
+                            Console.WriteLine("Informe um numero: ");
+                            int ano = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(this.model.bissexto(ano));
+                        break;
+                        case 10:
+                            Console.WriteLine("Informe um numero: ");
+                            int verificar = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(this.model.verificarNum(verificar));
+                        break;
+                        case 11:
+                            Console.WriteLine("Informe a idade:");
+                            int idade = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(this.model.verificarIdade(idade));
+                        break;
+                        case 12:
+                            Console.WriteLine("Informe um número: ");
+                            int intervalo = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Informe o primerio numero do intervalo: ");
+                            int intervaloComeco = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("Informe o segundo numero do intervalo: ");
+                            int intervaloFinal = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine(this.model.verificarIntervalo(intervalo, intervaloComeco, intervaloFinal));
+
+                        break;
+
+                        //exercicio 14
+                        case 14:
+                            Console.WriteLine(this.model.definirMaiorMenor());
+                        break;
                             default:
                                 Console.WriteLine("Informe um valor correto!!");
                             break;
--- CalculadoraTI23T/ModelCalculadora.cs
+++ CalculadoraTI23T/ModelCalculadora.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -80,35 +81,125 @@ namespace CalculadoraTI23T
 
         public double dividir()
         {
-            if (this.getNum2() == 0) 
+            if (this.getNum2() == 0)
             {
                 return -1; //indica que nao da pra dividir por 0
             }
-            else 
+            else
             {
                 this.setResultado(this.getNum1() / this.getNum2());
                 return this.getResultado();
             }
-                
+
         }
 
         //potencia
-        public double potencia() 
+        public double potencia()
         {
             this.setResultado(Math.Pow(this.getNum1(), this.getNum2()));
             return this.getResultado();
-        }
+        }//FIm do metodo
 
-        public double raizPrimeiroNumero() 
+        //Raiz do primeiro numero
+        public double raizPrimeiroNumero()
         {
             this.setResultado(Math.Sqrt(this.getNum1()));
             return this.getResultado();
-        }
+        }//fim do metodo
 
-        public double raizSegundoNumero() 
+        //Raiz do segundo numero
+        public double raizSegundoNumero()
         {
             this.setResultado(Math.Sqrt(this.getNum2()));
             return this.getResultado();
+        }//Fim do metodo
+
+        public string tabuada(int num)
+        {
+            string msg = "";
+            for (int i = 0; i <= 10; i++)
+            { 
+                msg += "\n" + num + " * " + i + " = " + (num* i);
+            }//Fim do for
+            return msg;
+        }//Fim do metodo
+
+        public string bissexto(int ano)
+        {
+
+            if ((ano % 4 == 0) || (ano % 4 == 0 && ano % 100 != 0))
+            {
+                return $"{ano} é bissexto";//Interpolação
+            }
+            else
+            {
+                return $"{ano} NÃO é bissexto";
+            }
+        }//fim do bissexto
+
+        public string verificarNum(int verificar) 
+        {
+            if (verificar < 100 || verificar > 200) 
+            {
+                return $"{verificar} nao esta entre 100 e 200";
+            }
+            else 
+            {
+                return $"{verificar} esta entre 100 e 200";
+            }
+        }
+
+        public string verificarIdade(int idade) 
+        {
+            if(idade < 16) 
+            {
+                return $"com a idade:{idade} voce nao pode votar";
+            }
+            else 
+            {
+                return $"com essa idade:{idade} voce pode votar";
+            }
+        }
+
+        public string verificarIntervalo(int intervalo , int intervaloComeco, int intervaloFinal) 
+        {
+            if (intervalo < intervaloComeco || intervalo > intervaloFinal) 
+            {
+                return $"O número:{intervalo} nao esta no intervalo";
+            }
+            else
+            {
+                return $"O numero:{intervalo} esta no intervalo";
+            }
+        }
+
+        //exercicio 14:
+
+        public string definirMaiorMenor() 
+        {
+            int num = 0;
+            int maior = 0;
+            int menor = 0;
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine((i+1) + "° Número");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                if(i == 0) 
+                {
+                    maior = num;
+                    menor = num;
+                }
+                if (num > maior) 
+                {
+                    maior = num;
+                }
+                if (num < menor) 
+                {
+                    menor = num;
+                }
+            }
+            return $" O maior é: {maior} e o menor é: {menor}";
         }
 
     }//fim da classe model
